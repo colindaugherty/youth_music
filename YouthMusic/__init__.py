d0d765b7c37af8fb86686d3923acfda562f16674
Colin <cdaug1228@gmail.com>
2017-02-25 10:51:09 -0500

    added the help page

--- YouthMusic/__init__.py
+++ YouthMusic/__init__.py
@@ -1,25 +1,25 @@
 from flask import Flask, flash, render_template, request, session, redirect, url_for, jsonify
-# import spotipy
-# from spotipy import oauth2
+import spotipy
+from spotipy import oauth2
 
-# #Variables
-# SP_client_id = 395c0c8b13eb418e9821084402274a16
-# SP_client_secret = a35b178c33ba49129168d6a16aeedf65
-# SPOTIPY_REDIRECT_URI = 'http://74.134.250.233/'
-# SCOPE = 'user-read-email,user-follow-modify,streaming,user-top-read,playlist-read-collaborative,'
-# CACHE = '.spotipyoauthcache'
+Variables
+SP_client_id = 395c0c8b13eb418e9821084402274a16
+SP_client_secret = a35b178c33ba49129168d6a16aeedf65
+SPOTIPY_REDIRECT_URI = 'http://74.134.250.233/'
+SCOPE = 'user-read-email,user-follow-modify,streaming,user-top-read,playlist-read-collaborative,'
+CACHE = '.spotipyoauthcache'
 
-# sp = spotipy.Spotipy()
+sp = spotipy.Spotipy()
 
 app = Flask(__name__)
 
 Flask.secret_key = "random"
 
-# @app.route("/search", methods=['POST'])
-# def search():
-#     return jsonify({
-#         'song': sp.search(q=request.form[song]) 
-#     })
+@app.route("/search", methods=['POST'])
+def search():
+    return jsonify({
+        'song': sp.search(q=request.form[song]) 
+    })
 
 @app.route("/")
 def index():
